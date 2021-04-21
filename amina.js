7c83d58188cadeb52c6f475d2c3aaf16af4c3ff4
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-04-21 06:28:20 +0600

    Create amina.js

---
+++ amina.js
@@ -0,0 +1,768 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
